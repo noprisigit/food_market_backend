4e82797835bc536a295b5cc998ed30dce0b9502c
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2021-11-23 11:54:57 +0700

    Create api for updating transaction

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -40,4 +40,13 @@ class TransactionController extends Controller
 
     return ResponseFormatter::success($transaction->paginate($limit), 'Data list transaksi berhasil diambil');
   }
+
+  public function update(Request $request, $id)
+  {
+    $transaction = Transaction::findOrFail($id);
+
+    $transaction->update($request->all());
+
+    return ResponseFormatter::success($transaction, 'Transaksi berhasil diperbarui');
+  }
 }
