f588a24976e623ae7824fbfe9a252f7ccbbebeb9
Sigit Prasetyo Noprianto <noprisigit@gmail.com>
2021-11-23 10:32:36 +0700

    Update food model

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Food extends Model
 {
-    use HasFactory;
+  use HasFactory;
+  use SoftDeletes;
+
+  protected $fillable = [
+    'name', 'description', 'ingredients', 'price', 'rate', 'type', 'picture_path'
+  ];
+
+  public function getCreatedAtAttribute($value)
+  {
+    return Carbon::parse($value)->timestamp;
+  }
+
+  public function getUpdatedAtAttribute($value)
+  {
+    return Carbon::parse($value)->timestamp;
+  }
 }
